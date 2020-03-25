f63ecd2c47e7407daff8ff9ee1f3328751e54196
danst3in <50937807+danst3in@users.noreply.github.com>
2020-03-25 15:37:40 -0700

    feat(dict): complete define map

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,29 @@
-export type Dict = {};
+// export type Dict = {};
+//
+// // Array.prototype.map, but for Dict
+// export function mapDict() {}
+//
+// // Array.prototype.reduce, but for Dict
+// export function reduceDict() {}
+
+export type Dict<T> = {
+  [key: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(
+  dict: Dict<T>,
+  fn: (arg: T, idx: number) => S
+): Dict<S> {
+  const out: Dict<S> = {};
+  Object.keys(dict).forEach((dKey, idx) => {
+    const thisItem = dict[dKey];
+    if (typeof thisItem !== 'undefined') {
+      out[dKey] = fn(thisItem, idx)
+    }
+  });
+  return out;
+}
 
 // Array.prototype.reduce, but for Dict
 export function reduceDict() {}
